87bf97d29480f63578ffc96205b5e3d73978a11b
Dario Heinze <heinzedario@gmail.com>
2025-05-27 16:19:29 -0400

    3er cambio

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -8,25 +8,35 @@ const CartItem = ({ onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
- 
-  };
+const calculateTotalAmount = () => {
+    let total = 0 ;
+    cart.forEach((item) => {
+        const {cost, quantity} = item ;
+        total += parseFloat(cost.substring(1)) * quantity ;
+    });
+    return total ;
+};
 
   const handleContinueShopping = (e) => {
    
   };
 
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
 
+    const handleIncrement = (item) => {
+        dispatch(updateQuantity({name: item.name, quantity: item.quantity+1}));
+    };
 
-  const handleIncrement = (item) => {
-  };
+    const handleDecrement = (item) => {
+        if (item.quantity > 0)
+            dispatch(updateQuantity({name: item.name, quantity: item.quantity-1}));
+    };
 
-  const handleDecrement = (item) => {
-   
-  };
-
-  const handleRemove = (item) => {
-  };
+    const handleRemove = (item) => {
+        dispatch(removeItem(item)) ;
+    };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
